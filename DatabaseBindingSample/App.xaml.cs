7aaff25a416bbec5362b19627e9e5eb60edb6d47
yehudah <yehudah.asher@gmail.com>
2015-06-15 09:25:12 +0300

    Project configuration

---
+++ DatabaseBindingSample/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace DatabaseBindingSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static App()
+        {
+            CultureInfo culture = CultureInfo.GetCultureInfo("en-US");
+            System.Threading.Thread.CurrentThread.CurrentCulture = culture;
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var viewModel = new ViewModels.MainViewModel();
+            var mainWindow = new MainWindow
+            {
+                DataContext = viewModel
+            };
+
+            this.MainWindow = mainWindow;
+            this.MainWindow.Show();
+        }
+    }
+}
